13d6eb84d660a26b7573f1d9ffcd6cc1602693cb
fernandalpez3 <ma.fernandalpez@gmail.com>
2017-08-31 14:16:42 -0500

    Kata 2 Ransom Note

---
+++ Kata_2/kata2.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+int canConstruct(char* ransomNote, char* magazine) {
+    int arr[128]={0x00};
+    char *m = magazine;
+    char *r =ransomNote;
+ //  printf("random %s\n", ransomNote);
+ //  printf("magazine %s\n", magazine);
+    while(*m)
+    {
+        arr[*m]++;
+        m++;
+       
+    }
+    while(*r)
+    {
+        if(!arr[*r])
+        {
+             return 0;
+        }
+        arr[*r]--;
+        r++;
+    }
+     return 1;
+     //printf("%i",result);
+
+}
