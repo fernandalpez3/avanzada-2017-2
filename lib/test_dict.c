49bf06473049eed4603052c40242d5ddfc816d08
fernandalpez3 <ma.fernandalpez@gmail.com>
2017-09-22 09:38:01 -0500

    dict and testlib

--- lib/test_dict.c
+++ lib/test_dict.c
@@ -3,68 +3,85 @@
 #include "minunit.h"
 #include "dict.h"
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+#define KYEL "\x1B[33m"
+#define KBLU "\x1B[34m"
+#define KMAG "\x1B[35m"
+#define KCYN "\x1B[36m"
+#define KWHT "\x1B[37m"
 #define RESET "\033[0m"
 
 int testsRun = 0;
 
-static char * testUpsertDictionary(){
+static char *testInitDictionary()
+{
+  unsigned int size = 4;
+  int errorCode;
+  Dict *myDictionary = initDictionary(size, &errorCode);
+  muAssert("myDictionary size must be 4", myDictionary->size == 4);
+  muAssert("myDictionary errorCode must be 0", errorCode == 0);
+  return 0;
+}
+
+static char *testUpsertDictionary()
+{
   unsigned int size = 1;
   int errorCode;
   Dict *myDictionary = initDictionary(size, &errorCode);
   int value = 1;
+  upsertDictionary(myDictionary, "uno", (void *)&value, sizeof(int), &errorCode);
 
-  upsertDictionary(myDictionary,"uno",(void *)&value, &errorCode); //& se usa para sacar la direccion de una variable, el * para apuntadores y para acceder a la direccion de memoria donde esta el apuntador 
-  
-  muAssert("my dictionary errorCode must be 0", errorCode == 0);
-  muAssert("my dictionary errorCode must be 0", strcmp(myDictionary->elements[0].key,"uno")== 0);
-  muAssert("my dictionary errorCode must be 0", *((int *)myDictionary->elements[0].value)== value);
-
+  muAssert("myDictionary errorCode must be 0", errorCode == 0);
+  muAssert("myDictionary errorCode must be 0", strcmp(myDictionary->elements[0].key, "uno") == 0);
+  muAssert("myDictionary errorCode must be 0", *((int *)myDictionary->elements[0].value) == value);
   return 0;
-
 }
 
-static char * testInitDictionary() {
-  unsigned int size = 4;
+static char *testGetDictionary()
+{
+  unsigned int size = 1;
   int errorCode;
   Dict *myDictionary = initDictionary(size, &errorCode);
-
-
-
-  muAssert("my dictionary must be 4", myDictionary->size == 4);
-  muAssert("my dictionary errorCode must be 0", errorCode == 0);
-
+  int value = 1;
+  upsertDictionary(myDictionary, "uno", (void *)&value,sizeof(int), &errorCode);
+  void *result = getDictionary(myDictionary, "uno", sizeof(int), &errorCode);
+  value = 27;
+  muAssert("myDictionary errorCode must be 0", errorCode == 0);
+  printf("%d\n", *((int *)result));
+  muAssert("result must be equal to value", *((int *)result) == 1);
   return 0;
 }
 
-static char * allTests() {
+static char *allTests()
+{
   muRunTest(testInitDictionary);
   muRunTest(testUpsertDictionary);
+  muRunTest(testGetDictionary);
   return 0;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   char *result = allTests();
-  if (result != 0) {
+  if (result != 0)
+  {
     printf("-_-_-_-_-_-_-_,------,      o \n");
     printf("_-_-_-_-_-_-_-|   /\\_/\\ \n");
     printf("-_-_-_-_-_-_-~|__( X .X)  +     + \n");
     printf("_-_-_-_-_-_-_- \"\"  \"\" \n");
     printf(KRED "✗ %s \n" RESET, result);
   }
-  else {
+  else
+  {
     printf("-_-_-_-_-_-_-_,------,      o \n");
     printf("_-_-_-_-_-_-_-|   /\\_/\\ \n");
     printf("-_-_-_-_-_-_-~|__( ^ .^)  +     + \n");
     printf("_-_-_-_-_-_-_-  \"\"  \"\" \n");
     printf(KGRN " ✓ ALL TESTS PASSED \n" RESET);
+
+    
   }
   printf("Tests run: %d\n", testsRun);
   return result != 0;
